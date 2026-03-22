3f009bd1cdebc0f2fd880be4a9874c2be1270749
Vorobjev Valery <vorobjevvalera@gmail.com>
2026-03-22 08:30:06 +0300

    feat: add simple linked list

---
+++ LeetCode/Outline/LinkedList.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Numerics;
+
+namespace LeetCode.Outline;
+
+/// <summary>
+/// Simple linked list
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class LinkedList<T> where T : IEqualityOperators<T, T, bool>
+{
+    private Node? Head { get; set; }
+
+    public void InsertBack(T value)
+    {
+        var newNode = new Node { Value = value, Next = null };
+        if (Head is null)
+        {
+            Head = newNode;
+            return;
+        }
+
+        var current = Head;
+        while (current!.HasNext())
+        {
+            current = current.Next;
+        }
+
+        current.Next = newNode;
+    }
+
+    public void InsertFront(T value)
+    {
+        var next = Head;
+        Head = new Node { Value = value, Next = next };
+    }
+
+    public void Delete(T value)
+    {
+        if (Head is null) return;
+
+        if (Head.Value == value)
+        {
+            Head = Head.Next;
+            return;
+        }
+
+        var current = Head.Next;
+        var previus = Head;
+        while (current is not null)
+        {
+            var next = current.Next;
+
+            if (current.Value == value)
+            {
+                previus.Next = next;
+                return;
+            }
+
+            previus = current;
+            current = next;
+        }
+
+
+    }
+
+    private Node? Search(T value)
+    {
+        if (Head is null) return null;
+
+        var current = Head;
+        while (current is not null)
+        {
+            if (current.Value == value) return current;
+            current = current.Next!;
+        }
+
+        return null;
+    }
+
+    private class Node
+    {
+        public required T Value { get; set; }
+        public Node? Next { get; set; }
+
+        public bool HasNext()
+        {
+            return Next is not null;
+        }
+    }
+}
